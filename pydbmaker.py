cd3d7744aa605025a20f339c6511ef7e8d28d3d4
Aaron Groch <ar.groch@gmail.com>
2021-02-27 17:11:40 -0800

    have db list and table list methods, working through creating a table method but not finished

--- pydbmaker.py
+++ pydbmaker.py
@@ -19,11 +19,10 @@ def main_menu():
 		db_create()
 	elif choice == 2:
 		clearscreen()
-		print("Let's modify that database!")
-		print("Under construction!")
-		sleep(1)
+		db_list()
+		sleep(3)
+		clearscreen()
 		main_menu()
-		# sql_connection(db_name)
 	elif choice == 3:
 		clearscreen()
 		print("Okay... see you later, then!")
@@ -50,7 +49,7 @@ def clearscreen():
 def db_create():
 	print("Let's create a database!")
 	db_name = input("Give your database a name: ")
-	current_db = sql_connection(db_name)
+	current_db = sql_connection(db_name + ".db")
 	print("Congrats! Your database (" + db_name + ") has been created!")
 	print("Please note: if a database by that name already existed, it will not have been duplicated.\nCheck 'Modify Database' to see if this database has existing tables.")
 	sleep(3)
@@ -63,7 +62,7 @@ def db_create_menu(db):
 	choice = int(input(""))
 	if choice == 1:
 		clearscreen()
-		modify_db(db)
+		db_modify(db)
 	elif choice == 2:
 		clearscreen()
 		main_menu()
@@ -76,18 +75,78 @@ def db_create_menu(db):
 		sleep(1)
 		db_create_menu(db)
 
-def modify_db(db):
-	print("Still working on this one. Try again later!")
-	sleep(3)
-	main_menu()
+# A method to list the databases in the project,
+# and allow user to choose one to modifying
+def db_list():
+	print("Which database do you want to modify?")
+	print("=====================================")
+	cur_dir = os.getcwd()
+	os.chdir(cur_dir)
+	count = 1
+	db_names = []
+	for file in glob.glob("*.db"):
+		print(str(count) + ". " + file)
+		count += 1
+		db_names.append(file)
+	choice = int(input("Choose database by number: "))
+	db_name = db_names[choice-1]
+	current_db = sql_connection(db_name)
+	db_modify(current_db)
+
+# A method for modifying a datbase
+def db_modify(db):	
+	cursorObj = db.cursor()
+	cursorObj.execute("SELECT name FROM sqlite_master WHERE type = 'table';")
+	table_list = cursorObj.fetchall() # list of the tables
+	if len(table_list) == 0:
+		# no tables
+		print("There currently no tables in this database.")
+		choice = input("Do you want to create a new one now? (y/n) ")
+		if choice == "y" or choice == "Y":
+			clearscreen()
+			table_name = new_table(db, cursorObj)
+		else:
+			clearscreen()
+			print("Well... then... to the Main Menu with you!")
+			sleep(1)
+			main_menu()
+	# there is at least one table:
+	else:
+		print("Current list of tables in this database:")
+		count = 1
+		table_choices = []
+		for table in table_list:
+			print(str(count) + ". " + table)
+			count += 1
+			table_choices.append(table)
+		choice = int(input("Choose table by number: "))
+		table_name = table_choices[choice-1]
+	# modify table here
+
+
+def new_table(db, cursorObj):
+	print("Let's create a new table in this database!")
+	table_name = input("Name the table: ")
+	number_of_fields = input("How many fields/columns will this table have? ")
+	count = 1
+	fields_arr = []
+	while count <= number_of_fields:
+		column_arr = [] # !!! Maybe better to create a Column class with properties of name, datatype, and primary_key
+		# ask for field name
+		# ask for data type
+		# ask if primary key; set as True or False
+		# create instance of Column,
+		# add to fields_arr
+
 
 
 # My secret testing area! Shhh!
 def testing():
-	cur_dir = os.getcwd()
-	os.chdir(cur_dir)
-	for file in glob.glob("*.py"):
-		print(file)
+
+	# cur_dir = os.getcwd()
+	# os.chdir(cur_dir)
+	# for file in glob.glob("*.py"):
+	# 	print(file)
 	sleep(3)
 	main_menu()
 
